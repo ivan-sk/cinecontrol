ed0377989bb65ad5ba88d29e31b262a0b18eaf27
Ivan S <i.skubko@gmail.com>
2024-01-08 20:36:21 +0200

    feat: added support for keyboard layouts

--- content.js
+++ content.js
@@ -4,11 +4,11 @@ chrome.runtime.onMessage.addListener(function (request) {
       const video = document.querySelector("video");
       const volumeStep = 0.1;
 
-      switch (event.key) {
-        case "u":
+      switch (event.code) {
+        case "KeyU":
           video.volume = Math.min(1, video.volume + volumeStep);
           break;
-        case "y":
+        case "KeyY":
           video.volume = Math.max(0, video.volume - volumeStep);
           break;
       }
